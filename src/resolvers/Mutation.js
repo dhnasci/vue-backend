dc9863c8f9661c66d781769a91c69b6c559e4916
Dirceu Henrique <dhnasci@hotmail.com>
2020-10-08 14:23:24 -0400

    Implementação do login com autenticação JWT

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,6 +3,26 @@ const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = process.env.JWT_SECRET
 
+async function login (_, {email, password}, ctx, info) {
+
+  const user = await ctx.db.query.user( { where: { email }})
+  if (!user){
+    throw new Error('Credencial de usuário inválida!')
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid){
+    throw new Error('Senha inválida!')
+  }
+
+  const token = jwt.sign( {userId: user.id}, JWT_SECRET, { expiresIn: '2h'}  )
+
+  return {
+    token,
+    user
+  }
+}
+
 async function signup (_, args, ctx, info) {
 
   const password = await bcrypt.hash(args.password, 10)
@@ -20,5 +40,6 @@ async function signup (_, args, ctx, info) {
 
 
 module.exports = {
-  signup
+  signup, 
+  login
 }
